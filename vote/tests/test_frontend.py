8e1ae5542ef4e636e54a060d4f7e8ccafb6cb329
Oleg Laktionov <ohlegik@gmail.com>
2021-11-07 19:41:14 +0300

    test Jenkinsfile

--- vote/tests/test_frontend.py
+++ vote/tests/test_frontend.py
@@ -21,6 +21,7 @@ class VoteUnitTest(unittest.TestCase):
         result = self.app.get('/')
         assert 'Version' in str(result.data)
 
+
     def test_processed_by_is_set(self):
         """Assert that processed by string is set"""
         result = self.app.get('/')
